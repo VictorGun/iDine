e70a15bc38ad10c25a9b87da95bb68f1ee05af9e
VictorGun <Thunderboomboom7@gmail.com>
2022-08-29 10:05:52 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Victor Gunderson on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
