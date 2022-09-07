f5720dca8cfc345f4b43d2d317f479b9102645b5
VictorGun <Thunderboomboom7@gmail.com>
2022-09-07 10:05:05 -0500

    finished with step 4

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
-    
+    let color: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text(String("$\(item.price)"))
+            }
+            
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(color[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
