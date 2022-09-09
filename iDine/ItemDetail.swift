0ac17943324fa4c45fc5cc993296988748815db3
VictorGun <Thunderboomboom7@gmail.com>
2022-09-09 09:46:30 -0500

    finished step 7

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ItemDetail: View {
     let item: MenuItem
-    
+    @EnvironmentObject var order: Order
     var body: some View {
         VStack {
             ZStack(alignment: .bottomTrailing) {
@@ -25,6 +25,12 @@ struct ItemDetail: View {
             }
             Text(item.description)
                 .padding()
+            
+            Button("Order This") {
+                order.add(item: item)
+            }
+            .font(.headline)
+            
             Spacer()
         }
         .navigationTitle(item.name)
@@ -35,7 +41,7 @@ struct ItemDetail: View {
 struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ItemDetail(item: MenuItem.example)
+            ItemDetail(item: MenuItem.example).environmentObject(Order())
         }
     }
 }
